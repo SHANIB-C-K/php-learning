c73951b17e675ac411dc1ce399071018093aea4b
SHANIB C K <134039228+SHANIBCK@users.noreply.github.com>
2023-12-20 00:08:45 +0530

    Code created sum.php

--- sum.php
+++ sum.php
@@ -1,1 +1,18 @@
+<?php
+// This is simplest program in php 
 
+//this program us add two numbers 
+
+//declaring section
+$num1 = 10;
+$num2 = 20;
+
+// Addition operation
+$sum = $num1 + $num2;
+
+//printing section
+echo "Sum is $sum";
+
+
+
+?>
