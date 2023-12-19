abdf0a26b2435a27d270c25e4107bfbc60663a0a
SHANIB C K <134039228+SHANIBCK@users.noreply.github.com>
2023-12-20 00:03:04 +0530

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,32 @@
+<?php
+
+//Php variables declare syntax is 
+//$VariableName = value;
+$num = 10; //its integers variable declare
+$name = "Zero Days"; //its declared in string variable 
+$float = 10.5; //its floating point variable declared 
+$bool = true; //its declared in boolean variable 
+
+echo "int is ".$num."\n"."String is ".$name."\n"."Float is ".$float."\n"."Boolean is ".$bool;
+// its print its variables
+
+
+
+
+
+
+/* output
+
+
+int is 10
+
+String is Zero Days
+
+Float is 10.5
+
+Boolean is 1
+
+
+*/
+
+?>
